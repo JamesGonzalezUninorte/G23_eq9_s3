26054635f534bcd4d08a093f8919282e5b3a7ee4
JamesGonzalezUninorte <jamesag@uninorte.edu.co>
2021-10-15 23:04:11 -0500

    commit a2110152304

---
+++ lst_s.py
@@ -0,0 +1,7 @@
+lst_Empleados=[
+    {'nombre':'Empleado Uno',  'id':'12345', 'cantidad_1':'15', 'cantidad_2':'123'},
+    {'nombre':'Empleado Dos',  'id':'12346', 'cantidad_1':'25', 'cantidad_2':'345'},
+    {'nombre':'Empleado Tres', 'id':'12347', 'cantidad_1':'35', 'cantidad_2':'567'},
+    {'nombre':'Empleado Cuatro', 'id':'12348', 'cantidad_1':'45', 'cantidad_2':'789'},
+    ]
+    
