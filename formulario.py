54091321632fb4810f908496187faa600a3c3241
JamesGonzalezUninorte <jamesag@uninorte.edu.co>
2021-10-27 10:23:31 -0500

    a211027_1023

--- formulario.py
+++ formulario.py
@@ -1,15 +1,4 @@
 from flask_wtf import FlaskForm
-<<<<<<< HEAD
-from flask_wtf.recaptcha import validators
-from wtforms import PasswordField, SubmitField, TextField
-# notafrom wtforms.fields.html5 import EmailField
-from wtforms.validators import InputRequired, EqualTo
-
-class pgLogin(FlaskForm):
-    user=TextField('Ususario *', validators=[InputRequired()])
-    passw=PasswordField('Contraseña *', validators=[InputRequired()])
-    ingresar=SubmitField('Ingresar')
-=======
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
@@ -19,4 +8,3 @@ class FormInicio(FlaskForm):
     recordar = BooleanField('Recordar Usuario')
     correo = StringField('Correo', validators=[DataRequired(message='No dejar vacío, completar')])
     enviar = SubmitField('Iniciar Sesión')
->>>>>>> d0c3532fc8fa5cef78b29e3b2c78954435471dff
